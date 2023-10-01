8174fead8e4f9618725bd774574396b05a24cb41
Rachael Caldwell <rachael.piper@cms.k12.nc.us>
2023-10-01 19:13:10 -0400

    upload python flask

---
+++ Resources/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Connect to the MongoDB database
+client = MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.5") 
+db = client["parks_db"]
+collection = db["park_data"]
+
+# Define a route to retrieve park data
+@app.route('/parks', methods=['GET'])
+def get_parks():
+    # Query the MongoDB collection and exclude the MongoDB _id field
+    parks = list(collection.find({}, {'_id': 0}))
+    return jsonify(parks)
+
+if __name__ == '__main__':
+    app.run(debug=True)
