dff8914cc9b91227f1e300735b938fa6b66b5757
Rachael Caldwell <rachael.piper@cms.k12.nc.us>
2023-10-02 10:01:25 -0400

    map test

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,29 +1,40 @@
-// Read in parks.json from the URL 
-const url = "https://gis.charlottenc.gov/arcgis/rest/services/HNS/HousingLocationalToolLayers/MapServer/10/query?outFields=*&where=1%3D1&f=geojson"
+// Wait for the DOM to be fully loaded before running the script
+document.addEventListener("DOMContentLoaded", function () {
+    // Define the URL for fetching park data
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://127.0.0.1:5000/';
 
-let map = L.map('map').setView([ 35.2271, -80.843124], 10);
+    // Create a Leaflet map and set the initial view
+    console.log("Initializing Leaflet map...");
+    const map = L.map('map').setView([35.2271, -80.843124], 10);
 
 // Add tile layer to the map
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);-80.843124
+}).addTo(map);
 
 // Create an empty array to hold park markers
 const parkMarkers = [];
 
 // Fetch park data from the URL
-fetch(url)
+fetch(proxyUrl + apiUrl)
     .then(response => response.json())
     .then(data => {
         // Loop through the park data and create markers for each park
-        data.features.forEach(park => {
-            const marker = L.marker([park.geometry.coordinates[1], park.geometry.coordinates[0]]).addTo(map);
+        data.forEach(parks => {
+            const latitude = parks.Latitude;
+            const longitude = parks.Longitude;
+            const marker = L.marker([latitude, longitude]).addTo(map);
             
             // Display the park name as a popup
-            marker.bindPopup(park.properties.NAME);
+            marker.bindPopup(parks.Park_name);
             
             // Add the marker to the parkMarkers array
             parkMarkers.push(marker);
         });
     })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  });
