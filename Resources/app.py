750328eaf85bf78dd77969abc2c1e5d02e9ba54d
Rachael Caldwell <rachael.piper@cms.k12.nc.us>
2023-10-02 18:20:10 -0400

    updated db with new lat/long

--- Resources/app.py
+++ Resources/app.py
@@ -7,8 +7,8 @@ CORS(app)
 
 # Connect to the MongoDB database
 client = MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.5") 
-db = client["parks_db"]
-collection = db["park_data"]
+db = client["parks_db2"]
+collection = db["parks"]
 
 # Define a route to retrieve park data
 @app.route('/', methods=['GET'])
@@ -19,5 +19,3 @@ def get_parks():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=5000)
-
-# API Link: http://127.0.0.1:5000/
