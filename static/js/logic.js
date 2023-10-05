75ab680e7cbac8ffe98b44ddb0688efda60fe205
NatkaBond <familybonko@gmail.com>
2023-10-05 16:44:01 -0400

    Added necessary files

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,9 +1,8 @@
-// Load the DOM
 document.addEventListener("DOMContentLoaded", function () {
     // Define the URL for fetching park data
     const url = 'http://127.0.0.1:5000/';
 
-    // Create a Leaflet map and set the initial view
+    // Initialize Leaflet map
     console.log("Initializing Leaflet map...");
     const map = L.map('map').setView([35.2271, -80.843124], 10);
 
@@ -13,177 +12,176 @@ document.addEventListener("DOMContentLoaded", function () {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    // Create a legend control and position it in the bottom right corner
+    // Create and add legend
     let legend = L.control({ position: "bottomright" });
     legend.onAdd = function (map) {
         const div = L.DomUtil.create("div", "legend");
-
-        // Define legend items
         const legendItems = [
             { label: "Playground", color: "green" },
             { label: "No Playground", color: "red" },
         ];
-
-        // Create legend items
         legendItems.forEach((item) => {
             const legendItem = L.DomUtil.create("div", "legend-item", div);
             const legendMarker = L.DomUtil.create("div", "legend-marker", legendItem);
             legendMarker.style.backgroundColor = item.color;
             L.DomUtil.create("span", "legend-label", legendItem).textContent = item.label;
         });
-    
         return div;
     };
-
-    // Add the legend to the map
     legend.addTo(map);
 
-    // Define an empty array to hold marker objects
+    // Marker array and park info update functions
     const markers = [];
-
-     // Display Individual Park Info When Selected
-     function updateParkInfo(park){
-        // Change Park Name When park marker is clicked
-        let element = document.getElementById("parkName");
-        element.innerHTML = park.Park_name;
-        // Change park link
-        element = document.getElementById("parkSite");
-        element.innerHTML = '<a href="'+park.parkurl +'">Website</a>'
-        // Change park Street Name
-        element = document.getElementById("parkStreet");
-        element.innerHTML = park.Address;
-        // Update park image
-        /*element = document.getElementById("parkMap");
-        element.innerHTML='<img src="ParkMaps/' + park.PARK_ID +'.png" alt="park image">';*/
+    function updateParkInfo(park) {
+        document.getElementById("parkName").innerHTML = park.Park_name;
+        document.getElementById("parkSite").innerHTML = '<a href="'+park.parkurl +'">Website</a>';
+        document.getElementById("parkStreet").innerHTML = park.Address;
     }
-    // Display Park Amenities
-    const parkAms = [" Restroom", " Camping", " Picnic Area", " Playground",
-        " Basketball", " Tennis", " Volleyball", " Walking Trail", " Dogpark",
-        " Garden", " Fitness", " Gazebo", " Play Swings", " Parking"]
-    
-    function displayAmenities(park){
+
+    // Amenity display and meter creation functions
+    const parkAms = ["Restroom", "Camping", "Picnic Area", "Playground",
+        "Basketball", "Tennis", "Volleyball", "Walking Trail", "Dogpark",
+        "Garden", "Fitness", "Gazebo", "Play Swings", "Parking"];
+
+    function displayAmenities(park) {
         let element = document.getElementById("amenities");
         let amenities_list = [];
-        let amenities_count = -1;
+        let amenities_count = 0; // Initialize count to 0
 
-        // Check for Amenities to Display
-        (park.restroom == "Yes") ? amenities_count++ && amenities_list.push(parkAms[0]): console.log("false");
-        (park.camping == "Yes") ? amenities_count++ && amenities_list.push(parkAms[1]): console.log("false");
-        (park.picnic == "Yes") ? amenities_count++ && amenities_list.push(parkAms[2]): console.log("false");
-        (park.playground == "Yes") ? amenities_count++ && amenities_list.push(parkAms[3]): console.log("false");
-        (park.basketball == "Yes") ? amenities_count++ && amenities_list.push(parkAms[4]): console.log("false");
-        (park.tennis == "Yes") ? amenities_count++ && amenities_list.push(parkAms[5]): console.log("false");
-        (park.volleyball == "Yes") ? amenities_count++ && amenities_list.push(parkAms[6]): console.log("false");
-        (park.walking == "Yes") ? amenities_count++ && amenities_list.push(parkAms[7]): console.log("false");
-        (park.dogpark == "Yes") ? amenities_count++ && amenities_list.push(parkAms[8]): console.log("false");
-        (park.garden == "Yes") ? amenities_count++ && amenities_list.push(parkAms[9]): console.log("false");
-        (park.fitness == "Yes") ? amenities_count++ && amenities_list.push(parkAms[10]): console.log("false");
-        (park.gazebo == "Yes") ? amenities_count++ && amenities_list.push(parkAms[11]): console.log("false");
-        (park.playswings == "Yes") ? amenities_count++ && amenities_list.push(parkAms[12]): console.log("false");
-        (park.parking == "Yes") ? amenities_count++ && amenities_list.push(parkAms[13]): console.log("false");
-        
-        // Display Amenities in a list, create li to append to list
-        for (i=0; i<amenities_list.length; i++){
-            let li = document.createElement('li');
-            li.innerText = amenities_list[i];
-            element.appendChild(li);
+        for (const [index, amenity] of parkAms.entries()) {
+            if (park[amenity.toLowerCase().replace(/ /g, "")] === "Yes") {
+                amenities_count++;
+                amenities_list.push(parkAms[index]);
+            }
         }
 
-        (amenities_count === -1) ? amenities_count = 0 : amenities_count = amenities_count;
+        // Update the amenities list display
+        element.innerHTML = "";  // Clear previous amenities
+        amenities_list.forEach(amenity => {
+            let li = document.createElement('li');
+            li.innerText = amenity;
+            element.appendChild(li);
+        });
+
         return amenities_count;
     }
 
-    // Fetch park data from the URL
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log("Fetched data:", data);
-            
-            // Function to find the coordinates of a city
-            function findCityCoordinates(cityName) {
-                for (const park of data) {
-                    if (park.City === cityName) {
-                        console.log(park.Latitude)
-                        return [park.Latitude, park.Longitude];
-                    }
-                }
-                return null;
+    function createAmenityMeter(count) {
+        const totalAmenities = 14;
+        const percentage = (count / totalAmenities) * 100;
+
+        return `
+            <div class="amenity-meter-bg">
+                <div class="amenity-meter-fill" style="width: ${percentage}%;" data-count="${count}"></div>
+            </div>
+            <div class="amenity-count">${count}/${totalAmenities} amenities</div>
+        `;
+    }
+
+   // Fetch park data
+fetch(url)
+.then(response => response.json())
+.then(data => {
+    function findCityCoordinates(cityName) {
+        for (const park of data) {
+            if (park.City === cityName) {
+                return [park.Latitude, park.Longitude];
             }
-            // Loop through the park data and create markers for each park
-            data.forEach(park => {
-                const latitude = park.Latitude;
-                const longitude = park.Longitude;
-                
-                // Determine the marker color according to playground or no playground
-                const markerColor = park.playground === "Yes" ? "green" : "red";
-                
-                // Create a marker with a custom icon
-                const marker = L.marker([latitude, longitude], {
-                    icon: L.divIcon({
-                        className: "custom-icon",
-                    html: `<div class="marker" style="background-color: ${markerColor};"></div>`
-                    })
-                });
-                
-                // Create a popup for each park
-                const popupContent = `<b>${park.Park_name}</b>`;
+        }
+        return null;
+    }
 
-                marker.bindPopup(popupContent);
-
-                // Store the park data with the marker
-                marker.park = park;
-
-                // Add the marker to the map
-                marker.addTo(map);
-
-                // Add Event Listener when marker is clicked
-                marker.addEventListener('click', event => {
-                    updateParkInfo(park);
-                    displayAmenities(park);
-                 });
-
-                // Store the marker in the markers array
-                markers.push(marker);
-            });
-
-            // Populate the dropdown with city values
-            const cities = [...new Set(data.map(park => park.City))];
-            const cityDropdown = document.getElementById("CityDropdown");
-
-            cities.forEach(city => {
-                const option = document.createElement("option");
-                option.value = city;
-                option.textContent = city;
-                cityDropdown.appendChild(option);
-            });
-
-            // Event listener for dropdown change
-            cityDropdown.addEventListener("change", function () {
-                const selectedCity = cityDropdown.value;
-                
-                // Update the selected city label
-                const selectedCityLabel = document.getElementById("SelectedCityLabel");
-
-                markers.forEach(marker => {
-                    const park = marker.park; // Access park data associated with the marker
-            
-                    if (selectedCity === "all") {
-                        // Show all markers when "all" is selected
-                        marker.setOpacity(1);
-                        map.setView([35.2271, -80.843124], 10);
-                    } else if (park.City === selectedCity) {
-                            // Show only markers that match the selected city
-                            marker.setOpacity(1);
-                        }
-                    else {
-                        // Hide all markers by default
-                        marker.setOpacity(0);
-                    }
-                });
-                map.setView(findCityCoordinates(selectedCity), 12);
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
+    // Create markers with popups
+    data.forEach(park => {
+        const markerColor = park.playground === "Yes" ? "green" : "red";
+        const marker = L.marker([park.Latitude, park.Longitude], {
+            icon: L.divIcon({
+                className: "custom-icon",
+                html: `<div class="marker" style="background-color: ${markerColor};"></div>`
+            })
         });
+
+        const amenitiesCount = displayAmenities(park);
+        const amenityMeterHtml = createAmenityMeter(amenitiesCount);
+        const popupContent = `<b>${park.Park_name}</b><br>${amenityMeterHtml}`;
+        marker.bindPopup(popupContent);
+        marker.park = park;
+        marker.addTo(map);
+        marker.addEventListener('click', () => {
+            updateParkInfo(park);
+            displayAmenities(park);
+        });
+        markers.push(marker);
+    });
+
+    // Populate the dropdown with city values
+    const cities = [...new Set(data.map(park => park.City))];
+    const cityDropdown = document.getElementById("CityDropdown");
+
+    cities.forEach(city => {
+        const option = document.createElement("option");
+        option.value = city;
+        option.textContent = city;
+        cityDropdown.appendChild(option);
+    });
+
+    // Event listener for dropdown change
+    cityDropdown.addEventListener("change", function () {
+        const selectedCity = cityDropdown.value;
+
+        markers.forEach(marker => {
+            const park = marker.park; // Access park data associated with the marker
+
+            if (selectedCity === "all") {
+                marker.setOpacity(1);
+                map.setView([35.2271, -80.843124], 10);
+            } else if (park.City === selectedCity) {
+                marker.setOpacity(1);
+            } else {
+                marker.setOpacity(0);
+            }
+        });
+        map.setView(findCityCoordinates(selectedCity), 12);
+    });
+
+    // Calculate the average amenities across all parks
+let totalAmenitiesCount = 0;
+data.forEach(park => {
+    totalAmenitiesCount += displayAmenities(park);
 });
+const averageAmenities = Math.round(totalAmenitiesCount / data.length);  // Round the average value
+
+// Create and display the average amenities meter
+const averageAmenityMeterHtml = createAmenityMeter(averageAmenities);
+const averageAmenityMeterDiv = document.createElement('div');
+averageAmenityMeterDiv.id = 'averageAmenityMeter';
+averageAmenityMeterDiv.innerHTML = `
+    <h3>Average Park Amenities</h3>
+    ${averageAmenityMeterHtml}
+`;
+document.body.appendChild(averageAmenityMeterDiv);
+
+})
+.catch(error => {
+    console.error('Error fetching data:', error);
+});
+});
+
+// Gauge Chart
+var data = [
+    {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: 2, // Updated value to 2
+        title: { text: "Park Amenities: 2/14" }, // Updated title to display "2/14"
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+            axis: {
+                range: [null, 14] // Set the maximum range of the gauge to 14
+            }
+        }
+    }
+];
+
+var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+Plotly.newPlot('gaugeDiv', data, layout);
